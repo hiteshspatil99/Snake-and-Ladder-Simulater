d4fac400eb989cd5d058a7a4d4f9d9f2d4fd335d
unknown <hiteshspatil99@gmail.com>
2021-11-10 22:31:12 +0530

    [Hitesh] add : Snake And Ladder Played At Position Zero

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
+        }
+    }
+}
---
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+        public int playerPosition = 0;
+    }
+}
